926f89542575053a5fc9baa0d74eba49c433f998
sashathelambo <166897058+sashathelambo@users.noreply.github.com>
2024-09-09 12:32:19 -0500

    Update TestGroceryCounter.java

--- src/test/java/com/csc/TestGroceryCounter.java
+++ src/test/java/com/csc/TestGroceryCounter.java
@@ -1,6 +1,6 @@
 package com.csc;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,7 +14,46 @@ public class TestGroceryCounter {
     counter = new GroceryCounter();
   }
 
-  // Put your tests here!
+  @Test
+  void testTens() {
+    counter.tens();
+    assertEquals("$10.00", counter.total());
+  }
+
+  @Test
+  void testOnes() {
+    counter.ones();
+    assertEquals("$1.00", counter.total());
+  }
+
+  @Test
+  void testTenths() {
+    counter.tenths();
+    assertEquals("$0.10", counter.total());
+  }
+
+  @Test
+  void testHundredths() {
+    counter.hundredths();
+    assertEquals("$0.01", counter.total());
+  }
+
+  @Test
+  void testOverflow() {
+    for (int i = 0; i < 100; i++) {
+      counter.tens();
+    }
+    assertEquals("$0.00", counter.total());
+    assertEquals(1, counter.number_of_overflows());
+  }
+
+  @Test
+  void testClear() {
+    counter.tens();
+    counter.clear();
+    assertEquals("$0.00", counter.total());
+    assertEquals(0, counter.number_of_overflows());
+  }
 
   @Test
   void itWorks() {
